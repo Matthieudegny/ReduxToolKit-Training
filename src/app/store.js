eafd0cd9f2af10faa966aec7ace5014f8cd0e95a
Matthieudegny <matthieu.dev.degny@gmail.com>
2022-07-25 20:25:10 +0200

    explanations store +

--- src/app/store.js
+++ src/app/store.js
@@ -9,18 +9,14 @@ export default configureStore({
 
 /*
 contrairemetn à redux avec son fichier Store.js dans le dossier Store:
+Redux = appel des middlewares + reducer qui est le fichier qui regroupe l'ensemble des reducers
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const enhancers = composeEnhancers(
   applyMiddleware(middlewareRecipes, middlewareSuggestions,middlewareFridge),
 );
-
 const store = createStore(reducer,enhancers);
-
 export default store;
-
-Redux = appel des middlewares + reducer qui est le fichier qui regroupe l'ensemble des reducers
 
 Toolkit = fichier store.js dans dossier app
 Ici ce sont les slices qui sont appelés qui representent "les reducers + actions" (à voir pour les middlewares avec createAsyncThunk)

40ad2cb3b2f9b010eb292c848e014a9b01734f0c
DEGNY <matthieu.degny.dev@gmail.com>
2022-07-25 19:53:20 +0200

    explanations store

--- src/app/store.js
+++ src/app/store.js
@@ -6,3 +6,23 @@ export default configureStore({
     pictures: picturesReducer,
   },
 });
+
+/*
+contrairemetn à redux avec son fichier Store.js dans le dossier Store:
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancers = composeEnhancers(
+  applyMiddleware(middlewareRecipes, middlewareSuggestions,middlewareFridge),
+);
+
+const store = createStore(reducer,enhancers);
+
+export default store;
+
+Redux = appel des middlewares + reducer qui est le fichier qui regroupe l'ensemble des reducers
+
+Toolkit = fichier store.js dans dossier app
+Ici ce sont les slices qui sont appelés qui representent "les reducers + actions" (à voir pour les middlewares avec createAsyncThunk)
+
+*/
